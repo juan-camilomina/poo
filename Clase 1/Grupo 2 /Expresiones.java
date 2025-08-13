6de3c65b3f90a6ff1e5db9ec0412cbf79a022a32
juan-camilomina <juan.minar@campusucc.edu.co>
2025-08-13 22:08:08 +0000

    expreciones clase 2

---
+++ Clase 1/Grupo 2 /Expresiones.java
@@ -0,0 +1,25 @@
+public class Expresiones {
+
+    public static void main(String [] args) {
+ 
+    double a = 4; 
+    double b = 5; 
+    double c = 1; 
+    double h = 0; 
+
+    h = (((b+ c) / 2 * a + 10 ) * 3 * b ) - 6; 
+    
+    System.out.println(" el resultado es : "+h);
+
+
+
+
+
+        System.out.println(b* a - b 
+
+   }
+
+
+}
+
+    
